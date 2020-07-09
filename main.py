c85d96b87d26414383a9860c84c24d57e1286880
Gabriel Silva Simoura <55196034+gabrielSSimoura@users.noreply.github.com>
2020-07-09 17:48:43 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+# Create a program that asks the user to enter their name and their age.
+# Print out a message addressed to them that tells them the year that they will turn 100 years old.
+
+# Extras:
+
+# Add on to the previous program by asking the user for another number and printing out that many copies of the previous message. (Hint: order of operations exists in Python)
+# Print out that many copies of the previous message on separate lines. (Hint: the string "\n is the same as pressing the ENTER button)
+
+def calc100years(age):
+    age = int(age) + 100
+    return str(age)
+
+
+def main():
+    userName = input("Name: ")
+    userAge = input("Age: ")
+
+    userAge = calc100years(userAge)
+
+    print(userName + " your age in 100 years will be " + userAge)
+
+
+main()
